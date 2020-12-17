2aac528fcea6911f9a13ec68afa7ef2998401087
Anand <cyberaj123@gmail.com>
2020-12-18 01:30:47 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var txtInput = document.querySelector("#txt-input");
+var translatebtn = document.querySelector("#btn-translate");
+var txtOutput = document.querySelector("#txt-output");
+
+//for writing value
+//outputTxt.innerHTML = "anand";
+
+//for writing value
+//.value
+
+let serverUrl = "https://api.funtranslations.com/translate/groot.json";
+
+function getTranslationURL(input) {
+  return serverUrl + "?" + "text=" + input;
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("something wrong with server! try again after some time");
+}
+
+function clickHandler() {
+  var inputText = txtInput.value;
+
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      txtOutput.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
+
+translatebtn.addEventListener("click", clickHandler);
